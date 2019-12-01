a928066cb8978171492489e2d60a513e39985034
Andreas <519178+schmidhauser@users.noreply.github.com>
2019-11-30 19:35:12 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # Copyright: (c) 2019 Andreas U. Schmidhauser <https://github.com/schmidhauser>
-# Thanks to Damien and Soren for the function.
 # License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 
 from . import number_of_decks
