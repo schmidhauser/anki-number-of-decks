fc4e8846a5048dcfad55ed16a13cac8b6190175b
Andreas <519178+schmidhauser@users.noreply.github.com>
2019-12-16 03:13:58 -0800

    License

--- number_of_decks.py
+++ number_of_decks.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright: (c) 2019 Andreas U. Schmidhauser <https://github.com/schmidhauser>
+# License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 
 """
 NUMBER OF DECKS
